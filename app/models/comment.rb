e18527780bc2d0d934d5a23d4a4e55f39fad2e88
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-29 17:51:24 -0300

    Added comment model validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
+  after_initialize :set_defaults
+
   belongs_to :user
   belongs_to :crypto
+
+  validates :content, presence: true
+
+  def set_defaults
+    self.is_edited ||= false
+  end
 end
