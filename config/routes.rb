0ae94606af39bbb47017c38f00237b2d644ef321
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-11-30 14:11:36 -0300

    Added the rest of controllers, basic routing and links on navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :cryptos, only: [:index, :show]
+  resources :users, only: [:index, :show]
+  get '/ranking', to: "users#index", as: :ranking
+  get '/wallet', to: "pages#wallet", as: :wallet
 end
