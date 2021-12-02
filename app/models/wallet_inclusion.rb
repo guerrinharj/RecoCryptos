640020c587301d196ca786072aced26b9f1712e8
Vinicio Gutierrez <vgutierrezdiniz@gmail.com>
2021-12-02 17:09:25 +0200

    created wallet inclusions table

---
+++ app/models/wallet_inclusion.rb
@@ -0,0 +1,4 @@
+class WalletInclusion < ApplicationRecord
+  belongs_to :user
+  belongs_to :crypto
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_132837) do
+ActiveRecord::Schema.define(version: 2021_12_02_150706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,7 +84,20 @@ ActiveRecord::Schema.define(version: 2021_11_30_132837) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "wallet_inclusions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "crypto_id", null: false
+    t.integer "buying_price"
+    t.integer "amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["crypto_id"], name: "index_wallet_inclusions_on_crypto_id"
+    t.index ["user_id"], name: "index_wallet_inclusions_on_user_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "cryptos"
   add_foreign_key "comments", "users"
+  add_foreign_key "wallet_inclusions", "cryptos"
+  add_foreign_key "wallet_inclusions", "users"
 end
