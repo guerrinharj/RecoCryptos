9425b64d0d130d0b8235dc76572bcdcd55652ba0
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2021-12-02 12:16:13 -0300

    Added like system

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :cryptos, only: [:index, :show, :update]
+  resources :cryptos, only: [:index, :show, :update] do
+    resources :likes, only: [:create]
+  end
+  resources :likes, only: [:destroy]
   resources :users, only: [:index, :show]
   get '/ranking', to: "users#index", as: :ranking
   get '/wallet', to: "pages#wallet", as: :wallet
