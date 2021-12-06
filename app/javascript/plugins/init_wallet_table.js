0da24cc3d065a720fe96fd6c7f5c2d45d690e83e
Vinicio Gutierrez <vgutierrezdiniz@gmail.com>
2021-12-06 15:11:23 +0200

    wallet unfinished

--- app/javascript/plugins/init_wallet_table.js
+++ app/javascript/plugins/init_wallet_table.js
@@ -12,5 +12,41 @@ const initWalletTable = () => {
       const dif = row.querySelector(".dif") //atualizar
       const profit = row.querySelector(".profit") //atualizar
       //const portfolio = row.querySelector(".portfolio") //atualizar
+
+      //updating costs
+      cost.innerText = parseInt(buying_price.innerText, 10) * parseInt(amount.innerText, 10)
+      //updating dif
+      let difference = (parseInt(price.innerText, 10) / parseInt(buying_price.innerText, 10) - 1) * 100
+      dif.innerText = Math.round(difference * 100) / 100
+      //updating profit
+      profit.innerText = (parseInt(price.innerText, 10) - parseInt(buying_price.innerText, 10)) * parseInt(amount.innerText, 10)
     })
+
+      //updating totalinvested
+      let costs = new Array
+      const costsArray = document.querySelectorAll(".cost")
+      costsArray.forEach((cost) => {
+        costs.push(cost)
+      })
+      costs = costs.map((element) => {
+       return parseInt(element.innerText, 10)
+      })
+      totalInvested.innerText = costs.reduce((previousValue, currentValue) => previousValue + currentValue)
+
+      //updating totalprofit
+      let profits = new Array
+      const profitsArray = document.querySelectorAll(".profit")
+      profitsArray.forEach((profit) => {
+        profits.push(profit)
+      })
+      profits = profits.map((element) => {
+        return parseInt(element.innerText, 10)
+      })
+      totalProfit.innerText = profits.reduce((previousValue, currentValue) => previousValue + currentValue)
+      console.log(totalProfit)
+      //updating totaldif
+      let totalDifference = parseInt(totalProfit.innerText, 10) / parseInt(totalInvested.innerText, 10) * 100
+      totalPercent.innerText = Math.round(totalDifference * 100) / 100
+
 }
+export { initWalletTable }
